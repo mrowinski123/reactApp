cc8cc30326f86e0759514e342c5b8a209d08e3d8
Maciej Rowiński <mrowinski@it-dev.pl>
2016-12-16 13:15:17 +0100

    added function tick componentDidMount, componenWillUnmount to update time every second

--- App.jsx
+++ App.jsx
@@ -78,7 +78,22 @@ class Clock extends React.Component {
         this.state = {date: new Date()};
     }
 
-    
+    componentDidMount() {
+        this.timerID = setInterval(
+            () => this.tick(),
+            1000
+        );
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.timerID);
+    }
+
+    tick() {
+        this.setState({
+            date: new Date()
+        });
+    }
 
     render() {
         return(
